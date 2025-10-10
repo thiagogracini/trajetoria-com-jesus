7831067241001a489ad84c06d751434c7d0a83a9
Thiago Gracini <dev@thiagogracini.com.br>
2025-10-09 21:50:47 -0300

    change lead from newsletter subscription page

--- app/components/lp-newsletter/hero.js
+++ app/components/lp-newsletter/hero.js
@@ -5,14 +5,14 @@ export function Hero() {
   return (
     <section className="laptop:h-screen h-full bg-[#fff7d2] pb-88 pt-88">
       <ContainerGrid className="text-center">
-        <h2 className="text-[38px]">Newsletter</h2>
+        <h2 className="text-[38px]">A Newsletter</h2>
         <h1 className="font-redhatdisplay text-[51px] font-bold">
           Trajetória com Jesus
         </h1>
         <p className="font-alegreya text-[37px] pb-48">
-          É mais do que um devocional diário, é{" "}
-          <strong>um guia para a vida espiritual, vida pessoal</strong> e{" "}
-          <strong>vida profissional</strong>
+          É muito mais do que um devocional diário, é{" "}
+          <strong>um guia para a vida espiritual, pessoal</strong> e{" "}
+          <strong>profissional</strong>
         </p>
         <p className="text-[37px] mb-24">
           <strong>Inscreva-se</strong> gratuitamente
