f6a66e57884e6a1444b2aa7e83260588f4e5e2a0
Thiago Gracini <dev@thiagogracini.com.br>
2025-10-13 19:18:16 -0300

    add page `confirmacao`

--- app/agradecimento/confirmacao/page.js
+++ app/agradecimento/confirmacao/page.js
@@ -5,13 +5,11 @@ export default function ThanksPage() {
         Cartas <strong>Ao Cristão</strong>
       </h2>
       <h1 className="text-[36px] text-center font-bold pb-32">
-        Obrigado por se inscrever
+        Inscrição Confirmada
       </h1>
-      <p className="text-[20px] m-auto text-center pb-32 laptop:pb-42 pl-15 pr-15 max-w-620">
-        Fique de olho no seu e-mail. Se não encontrar minha mensagem, ela pode
-        estar na caixa de <strong>spam ou promoções</strong>. Assim que receber,
-        já me coloque nos favoritos. Assim você garante que vai receber todas as
-        minhas cartas, combinado? Até mais!
+      <p className="text-[20px] laptop:text-[24px] m-auto text-center pb-32 laptop:pb-42 pl-15 pr-15 max-w-620">
+        Espero que você aproveite e que cada carta enviada possa te ajudar a
+        guiar sua vida.
       </p>
       <p className="text-[36px] font-redhatdisplay m-auto text-center max-w-160">
         fique com <strong>DEUS</strong>
